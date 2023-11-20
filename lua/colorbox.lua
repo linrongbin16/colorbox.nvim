0541ea59ce0050b664775d2fb1d2aae575547145
linrongbin16 <linrongbin16@outlook.com>
2023-11-20 17:03:29 +0800

    WIP

--- lua/colorbox.lua
+++ lua/colorbox.lua
@@ -16,14 +16,65 @@ local Defaults = {
 --- @type colorbox.Options
 local Configs = {}
 
-local function build()
+--- @class ColorSpec
+--- @field name string
+--- @field path string
+--- @field colors string[]
+local ColorSpec = {}
+
+--- @param name string
+--- @param path string
+--- @param colors string[]|nil
+function ColorSpec:new(name, path, colors)
+    local o = {
+        name = name,
+        path = path,
+        colors = colors or {},
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+local ColorSpecs = {}
+local ColorSpecsMap = {}
+
+local function build_index()
     local cwd = vim.fn["colorbox#base_dir"]()
-    logger.debug("|colorbox.build| cwd:%s", vim.inspect(cwd))
+    local packopt = string.format("%s/pack/colorbox/opt", cwd)
+    logger.debug(
+        "|colorbox.build| cwd:%s, pack:%s",
+        vim.inspect(cwd),
+        vim.inspect(packopt)
+    )
     vim.opt.packpath:append(cwd)
     -- vim.opt.packpath:append(cwd .. "/pack")
     -- vim.opt.packpath:append(cwd .. "/pack/colorbox")
     -- vim.opt.packpath:append(cwd .. "/pack/colorbox/opt")
     -- vim.cmd([[packadd catppuccin-nvim]])
+
+    local pack_dir = vim.loop.fs_opendir(packopt) --[[@as luv_dir_t]]
+    while true do
+        local tmp = pack_dir:readdir()
+        if type(tmp) == "table" and #tmp > 0 then
+            for _, ttmp in ipairs(tmp) do
+                if
+                    type(ttmp) == "table"
+                    and type(ttmp.name) == "string"
+                    and ttmp.type == "directory"
+                then
+                    local spec = ColorSpec:new(
+                        ttmp.name,
+                        string.format("%s/%s", packopt, ttmp.name)
+                    )
+                    table.insert(ColorSpecs, spec)
+                    ColorSpecsMap[spec.name] = spec
+                end
+            end
+        else
+            break
+        end
+    end
 end
 
 --- @param opts colorbox.Options?
@@ -38,7 +89,7 @@ local function setup(opts)
         file_log_name = "colorbox.log",
     })
 
-    build()
+    build_index()
 end
 
 local M = { setup = setup }
