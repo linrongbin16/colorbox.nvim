8f0db136bd2d78f58b9fb0ca98b8589babd99fe3
linrongbin16 <linrongbin16@outlook.com>
2023-11-20 15:28:24 +0800

    WIP

--- lua/colorbox.lua
+++ lua/colorbox.lua
@@ -1,6 +1,5 @@
 local logger = require("colorbox.logger")
 local LogLevels = require("colorbox.logger").LogLevels
-local colors = require("colorbox.colors")
 
 --- @alias colorbox.Options table<any, any>
 --- @type colorbox.Options
@@ -16,6 +15,50 @@ local Defaults = {
 --- @type colorbox.Options
 local Configs = {}
 
+local function build()
+    local cwd = vim.fn["colorbox#base_dir"]()
+    logger.debug("|colorbox.scan_modules| cwd:%s", vim.inspect(cwd))
+    local modules_dir = vim.loop.fs_opendir(cwd .. "/modules") --[[@as luv_dir_t]]
+    local modules = {}
+    local modules_path = {}
+    local n = 0
+    while true do
+        local m = modules_dir:readdir()
+        if type(m) == "table" and #m > 0 then
+            for _, mm in ipairs(m) do
+                if
+                    type(mm) == "table"
+                    and type(mm.name) == "string"
+                    and string.len(mm.name) > 0
+                    and mm.type == "directory"
+                then
+                    -- logger.debug(
+                    --     "|colorbox.scan_modules| %d:%s",
+                    --     n + 1,
+                    --     vim.inspect(m)
+                    -- )
+                    table.insert(modules, mm)
+                    table.insert(
+                        modules_path,
+                        string.format("%s/modules/%s", cwd, mm.name)
+                    )
+                    n = n + 1
+                end
+            end
+        else
+            break
+        end
+    end
+    -- logger.debug("|colorbox.scan_modules| modules:%s", vim.inspect(modules))
+    logger.debug(
+        "|colorbox.scan_modules| modules_path:%s",
+        vim.inspect(modules_path)
+    )
+    for _, p in ipairs(modules_path) do
+        vim.opt.runtimepath:append(p)
+    end
+end
+
 --- @param opts colorbox.Options?
 local function setup(opts)
     Configs = vim.tbl_deep_extend("force", vim.deepcopy(Defaults), opts or {})
@@ -28,7 +71,7 @@ local function setup(opts)
         file_log_name = "colorbox.log",
     })
 
-    colors.setup()
+    build()
 end
 
 local M = { setup = setup }
