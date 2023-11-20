ed82dde45c26dce51602af7b6dff4e089e7711f7
linrongbin16 <linrongbin16@outlook.com>
2023-11-20 16:47:22 +0800

    WIP (#17)

--- lua/colorbox.lua
+++ lua/colorbox.lua
@@ -1,5 +1,6 @@
 local logger = require("colorbox.logger")
 local LogLevels = require("colorbox.logger").LogLevels
+local json = require("colorbox.json")
 
 --- @alias colorbox.Options table<any, any>
 --- @type colorbox.Options
@@ -18,6 +19,11 @@ local Configs = {}
 local function build()
     local cwd = vim.fn["colorbox#base_dir"]()
     logger.debug("|colorbox.build| cwd:%s", vim.inspect(cwd))
+    vim.opt.packpath:append(cwd)
+    -- vim.opt.packpath:append(cwd .. "/pack")
+    -- vim.opt.packpath:append(cwd .. "/pack/colorbox")
+    -- vim.opt.packpath:append(cwd .. "/pack/colorbox/opt")
+    -- vim.cmd([[packadd catppuccin-nvim]])
 end
 
 --- @param opts colorbox.Options?
