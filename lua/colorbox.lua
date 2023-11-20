1d7fe09351a07fde9c0fde7f379da8a278cfe35c
linrongbin16 <linrongbin16@outlook.com>
2023-11-20 17:19:14 +0800

    WIP

--- lua/colorbox.lua
+++ lua/colorbox.lua
@@ -2,6 +2,44 @@ local logger = require("colorbox.logger")
 local LogLevels = require("colorbox.logger").LogLevels
 local json = require("colorbox.json")
 
+--- @param s string
+--- @param t string
+--- @return boolean
+local function string_endswith(s, t)
+    return string.len(s) >= string.len(t) and s:sub(#s - #t + 1) == t
+end
+
+--- @param s string
+--- @param t string
+--- @param start integer?
+--- @return integer?
+local function string_find(s, t, start)
+    -- start = start or 1
+    -- local result = vim.fn.stridx(s, t, start - 1)
+    -- return result >= 0 and (result + 1) or nil
+
+    start = start or 1
+    for i = start, #s do
+        local match = true
+        for j = 1, #t do
+            if i + j - 1 > #s then
+                match = false
+                break
+            end
+            local a = string.byte(s, i + j - 1)
+            local b = string.byte(t, j)
+            if a ~= b then
+                match = false
+                break
+            end
+        end
+        if match then
+            return i
+        end
+    end
+    return nil
+end
+
 --- @alias colorbox.Options table<any, any>
 --- @type colorbox.Options
 local Defaults = {
@@ -39,7 +77,7 @@ end
 local ColorSpecs = {}
 local ColorSpecsMap = {}
 
-local function build_index()
+local function build_specs()
     local cwd = vim.fn["colorbox#base_dir"]()
     local packopt = string.format("%s/pack/colorbox/opt", cwd)
     logger.debug(
@@ -55,20 +93,61 @@ local function build_index()
 
     local pack_dir = vim.loop.fs_opendir(packopt) --[[@as luv_dir_t]]
     while true do
-        local tmp = pack_dir:readdir()
-        if type(tmp) == "table" and #tmp > 0 then
-            for _, ttmp in ipairs(tmp) do
+        local pack_tmp = pack_dir:readdir()
+        if type(pack_tmp) == "table" and #pack_tmp > 0 then
+            for i, pack_ttmp in ipairs(pack_tmp) do
                 if
-                    type(ttmp) == "table"
-                    and type(ttmp.name) == "string"
-                    and ttmp.type == "directory"
+                    type(pack_ttmp) == "table"
+                    and type(pack_ttmp.name) == "string"
+                    and pack_ttmp.type == "directory"
                 then
                     local spec = ColorSpec:new(
-                        ttmp.name,
-                        string.format("%s/%s", packopt, ttmp.name)
+                        pack_ttmp.name,
+                        string.format("%s/%s", packopt, pack_ttmp.name)
                     )
                     table.insert(ColorSpecs, spec)
                     ColorSpecsMap[spec.name] = spec
+                    local color_dir, err =
+                        vim.loop.fs_opendir(spec.path .. "/colors") --[[@as luv_dir_t]]
+                    if not color_dir then
+                        logger.err(
+                            "failed to scan %s directory: %s",
+                            vim.inspect(spec.path),
+                            vim.inspect(err)
+                        )
+                    end
+                    while true do
+                        local color_tmp = color_dir:readdir()
+                        if type(color_tmp) == "table" and #color_tmp > 0 then
+                            for j, color_ttmp in ipairs(color_tmp) do
+                                logger.debug(
+                                    "|colorbox.build_specs| colors_ttmp %d:%s",
+                                    j,
+                                    vim.inspect(color_ttmp)
+                                )
+                                if
+                                    type(color_ttmp) == "table"
+                                    and type(color_ttmp.name) == "string"
+                                    and color_ttmp.type == "file"
+                                then
+                                    local color_file = color_ttmp.name
+                                    assert(
+                                        string_endswith(color_file, ".vim")
+                                            or string_endswith(
+                                                color_file,
+                                                ".lua"
+                                            )
+                                    )
+                                    local color_name =
+                                        color_file:sub(1, #color_file - 4)
+                                    table.insert(spec.colors, color_name)
+                                end
+                            end
+                        else
+                            break
+                        end
+                    end
+                    vim.cmd(string.format([[packadd %s]], spec.name))
                 end
             end
         else
@@ -89,7 +168,7 @@ local function setup(opts)
         file_log_name = "colorbox.log",
     })
 
-    build_index()
+    build_specs()
 end
 
 local M = { setup = setup }
