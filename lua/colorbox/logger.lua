2964347c613ad7e47103067a0f09f2c0953a75c6
linrongbin16 <linrongbin16@outlook.com>
2023-11-22 11:52:25 +0800

    feat(logs): file logging 'w' mode

--- lua/colorbox/logger.lua
+++ lua/colorbox/logger.lua
@@ -38,6 +38,8 @@ local Defaults = {
     file_log_name = nil,
     file_log_dir = vim.fn.stdpath("data"),
     _file_log_path = nil,
+    --- @type "a"|"w"
+    _file_log_mode = "a",
 }
 
 local Configs = {}
@@ -70,6 +72,10 @@ local function setup(opts)
         assert(type(Configs.file_log_dir) == "string")
         assert(string.len(Configs.file_log_dir) > 0)
         assert(vim.fn.isdirectory(Configs.file_log_dir) > 0)
+        assert(Configs._file_log_mode == "a" or Configs._file_log_mode == "w")
+        if Configs._file_log_mode == "w" then
+            Configs._file_handle = io.open(Configs._file_log_path, "w")
+        end
     end
 end
 
@@ -111,7 +117,12 @@ local function log(level, msg)
         echo(level, msg)
     end
     if Configs.file_log then
-        local fp = io.open(Configs._file_log_path, "a")
+        local fp = nil
+        if Configs._file_log_mode == "a" then
+            fp = io.open(Configs._file_log_path, "a")
+        else
+            fp = Configs._file_handle
+        end
         if fp then
             for _, line in ipairs(msg_lines) do
                 fp:write(
@@ -123,7 +134,11 @@ local function log(level, msg)
                     )
                 )
             end
-            fp:close()
+        end
+        if Configs._file_log_mode == "a" then
+            if fp then
+                fp:close()
+            end
         end
     end
 end
