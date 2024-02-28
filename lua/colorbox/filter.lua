797f8cc8828fa66ea19bef7a78cd55149b06df8a
linrongbin16 <linrongbin16@users.noreply.github.com>
2024-02-28 03:23:00 +0000

    chore(pr): auto-commit

--- lua/colorbox/filter.lua
+++ lua/colorbox/filter.lua
@@ -10,10 +10,10 @@ local M = {}
 --- @param spec colorbox.ColorSpec
 --- @return boolean
 M._builtin_filter = function(f, color_name, spec)
-    if f == "primary" then
-        return builtin_filters.primary(color_name, spec)
-    end
-    return false
+  if f == "primary" then
+    return builtin_filters.primary(color_name, spec)
+  end
+  return false
 end
 
 --- @param f colorbox.FunctionFilterConfig
@@ -21,17 +21,15 @@ end
 --- @param spec colorbox.ColorSpec
 --- @return boolean
 M._function_filter = function(f, color_name, spec)
-    if type(f) == "function" then
-        local ok, result = pcall(f, color_name, spec)
-        if ok and type(result) == "boolean" then
-            return result
-        else
-            logging
-                .get("colorbox")
-                :err("failed to invoke function filter, please check your config!")
-        end
+  if type(f) == "function" then
+    local ok, result = pcall(f, color_name, spec)
+    if ok and type(result) == "boolean" then
+      return result
+    else
+      logging.get("colorbox"):err("failed to invoke function filter, please check your config!")
     end
-    return false
+  end
+  return false
 end
 
 --- @param f colorbox.AllFilterConfig
@@ -39,37 +37,37 @@ end
 --- @param spec colorbox.ColorSpec
 --- @return boolean
 M._all_filter = function(f, color_name, spec)
-    if type(f) == "table" then
-        for _, f1 in ipairs(f) do
-            if type(f1) == "string" then
-                local result = M._builtin_filter(f1, color_name, spec)
-                if not result then
-                    return result
-                end
-            elseif type(f1) == "function" then
-                local result = M._function_filter(f1, color_name, spec)
-                if not result then
-                    return result
-                end
-            end
+  if type(f) == "table" then
+    for _, f1 in ipairs(f) do
+      if type(f1) == "string" then
+        local result = M._builtin_filter(f1, color_name, spec)
+        if not result then
+          return result
         end
+      elseif type(f1) == "function" then
+        local result = M._function_filter(f1, color_name, spec)
+        if not result then
+          return result
+        end
+      end
     end
-    return true
+  end
+  return true
 end
 
 --- @param color_name string
 --- @param spec colorbox.ColorSpec
 --- @return boolean
 M.run = function(color_name, spec)
-    local confs = configs.get()
-    if type(confs.filter) == "string" then
-        return M._builtin_filter(confs.filter, color_name, spec)
-    elseif type(confs.filter) == "function" then
-        return M._function_filter(confs.filter, color_name, spec)
-    elseif type(confs.filter) == "table" then
-        return M._all_filter(confs.filter, color_name, spec)
-    end
-    return false
+  local confs = configs.get()
+  if type(confs.filter) == "string" then
+    return M._builtin_filter(confs.filter, color_name, spec)
+  elseif type(confs.filter) == "function" then
+    return M._function_filter(confs.filter, color_name, spec)
+  elseif type(confs.filter) == "table" then
+    return M._all_filter(confs.filter, color_name, spec)
+  end
+  return false
 end
 
 return M
