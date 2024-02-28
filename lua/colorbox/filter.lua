6e75b540f5de6464fb5d44f97eab930eb79003ae
linrongbin16 <linrongbin16@outlook.com>
2024-02-28 11:19:15 +0800

    chore

--- lua/colorbox/filter.lua
+++ lua/colorbox/filter.lua
@@ -9,7 +9,7 @@ local M = {}
 --- @param color_name string
 --- @param spec colorbox.ColorSpec
 --- @return boolean
-local function _builtin_filter(f, color_name, spec)
+M._builtin_filter = function(f, color_name, spec)
     if f == "primary" then
         return builtin_filters.primary(color_name, spec)
     end
@@ -20,7 +20,7 @@ end
 --- @param color_name string
 --- @param spec colorbox.ColorSpec
 --- @return boolean
-local function _function_filter(f, color_name, spec)
+M._function_filter = function(f, color_name, spec)
     if type(f) == "function" then
         local ok, result = pcall(f, color_name, spec)
         if ok and type(result) == "boolean" then
@@ -38,16 +38,16 @@ end
 --- @param color_name string
 --- @param spec colorbox.ColorSpec
 --- @return boolean
-local function _all_filter(f, color_name, spec)
+M._all_filter = function(f, color_name, spec)
     if type(f) == "table" then
         for _, f1 in ipairs(f) do
             if type(f1) == "string" then
-                local result = _builtin_filter(f1, color_name, spec)
+                local result = M._builtin_filter(f1, color_name, spec)
                 if not result then
                     return result
                 end
             elseif type(f1) == "function" then
-                local result = _function_filter(f1, color_name, spec)
+                local result = M._function_filter(f1, color_name, spec)
                 if not result then
                     return result
                 end
@@ -63,11 +63,11 @@ end
 M.run = function(color_name, spec)
     local confs = configs.get()
     if type(confs.filter) == "string" then
-        return _builtin_filter(confs.filter, color_name, spec)
+        return M._builtin_filter(confs.filter, color_name, spec)
     elseif type(confs.filter) == "function" then
-        return _function_filter(confs.filter, color_name, spec)
+        return M._function_filter(confs.filter, color_name, spec)
     elseif type(confs.filter) == "table" then
-        return _all_filter(confs.filter, color_name, spec)
+        return M._all_filter(confs.filter, color_name, spec)
     end
     return false
 end
--- spec/colorbox/filter_spec.lua
+++ spec/colorbox/filter_spec.lua
@@ -12,11 +12,31 @@ describe("colorbox", function()
     local colorbox = require("colorbox")
     local db = require("colorbox.db")
     local filter = require("colorbox.filter")
+    local builtin_filter = require("colorbox.filter.builtin")
     colorbox.setup({
         debug = true,
         file_log = true,
     })
 
+    describe("[builtin_filter]", function()
+        it("primary", function()
+            local ColorNameToColorSpecsMap = db.get_color_name_to_color_specs_map()
+            local input_color = "tokyonight"
+            local input_spec = ColorNameToColorSpecsMap[input_color]
+            for _, c in ipairs(input_spec.color_names) do
+                local actual = builtin_filter.primary(c, input_spec)
+                print(
+                    string.format(
+                        "input color:%s, current color:%s, actual:%s\n",
+                        vim.inspect(input_color),
+                        vim.inspect(c),
+                        vim.inspect(actual)
+                    )
+                )
+                assert_eq(actual, input_color == c)
+            end
+        end)
+    end)
     describe("filter", function()
         it("run", function()
             local ColorNameToColorSpecsMap = db.get_color_name_to_color_specs_map()
@@ -24,6 +44,59 @@ describe("colorbox", function()
                 local actual = filter.run(color, spec)
                 assert_eq(type(actual), "boolean")
             end
+        end)
+        describe("[filter]", function()
+            it("_builtin_filter", function()
+                local ColorNameToColorSpecsMap = db.get_color_name_to_color_specs_map()
+                for color, spec in pairs(ColorNameToColorSpecsMap) do
+                    local actual = filter._builtin_filter("primary", color, spec)
+                    assert_eq(type(actual), "boolean")
+                end
+            end)
+            it("_function_filter", function()
+                local ColorNameToColorSpecsMap = db.get_color_name_to_color_specs_map()
+                for color, spec in pairs(ColorNameToColorSpecsMap) do
+                    local actual = filter._function_filter(function(c, s)
+                        return true
+                    end, color, spec)
+                    assert_eq(type(actual), "boolean")
+                    assert_true(actual)
+                end
+                for color, spec in pairs(ColorNameToColorSpecsMap) do
+                    local actual = filter._function_filter(function(c, s)
+                        return false
+                    end, color, spec)
+                    assert_eq(type(actual), "boolean")
+                    assert_false(actual)
+                end
+            end)
+            it("_all_filter", function()
+                local ColorNameToColorSpecsMap = db.get_color_name_to_color_specs_map()
+                for color, spec in pairs(ColorNameToColorSpecsMap) do
+                    local actual = filter._all_filter({
+                        function(c, s)
+                            return true
+                        end,
+                        function(c, s)
+                            return true
+                        end,
+                    }, color, spec)
+                    assert_eq(type(actual), "boolean")
+                    assert_true(actual)
+                end
+                for color, spec in pairs(ColorNameToColorSpecsMap) do
+                    local actual = filter._all_filter({
+                        function(c, s)
+                            return false
+                        end,
+                        function(c, s)
+                            return false
+                        end,
+                    }, color, spec)
+                    assert_eq(type(actual), "boolean")
+                    assert_false(actual)
+                end
+            end)
         end)
     end)
 end)
