7ccdbddc54340240622e7ce9c2fa890bce4899fc
linrongbin16 <linrongbin16@outlook.com>
2024-02-29 13:23:35 +0800

    perf(startup): improve color rendering on nvim start

---
+++ lua/colorbox/loader.lua
@@ -0,0 +1,46 @@
+local logging = require("colorbox.commons.logging")
+local LogLevels = require("colorbox.commons.logging").LogLevels
+local strings = require("colorbox.commons.strings")
+local tables = require("colorbox.commons.tables")
+
+local configs = require("colorbox.configs")
+local timing = require("colorbox.timing")
+local track = require("colorbox.track")
+local runtime = require("colorbox.runtime")
+
+local M = {}
+
+--- @param colorname string?
+M.load = function(colorname)
+  local ColorNameToColorSpecsMap = require("colorbox.db").get_color_name_to_color_specs_map()
+
+  if strings.empty(colorname) then
+    return
+  end
+  local spec = ColorNameToColorSpecsMap[colorname]
+  if tables.tbl_empty(spec) then
+    return
+  end
+
+  local autoload_path = string.format("%s/autoload", spec.full_pack_path)
+  vim.opt.runtimepath:append(autoload_path)
+  vim.cmd(string.format([[packadd %s]], spec.git_path))
+
+  local confs = configs.get()
+  local logger = logging.get("colorbox") --[[@as commons.logging.Logger]]
+  if type(confs.setup) == "table" and vim.is_callable(confs.setup[spec.handle]) then
+    local home_dir = vim.fn["colorbox#base_dir"]()
+    local ok, setup_err = pcall(confs.setup[spec.handle], home_dir, spec)
+    logger:ensure(
+      ok,
+      "failed to setup colorscheme:%s, error:%s",
+      vim.inspect(spec.handle),
+      vim.inspect(setup_err)
+    )
+  end
+  if confs.background == "dark" or confs.background == "light" then
+    vim.opt.background = confs.background
+  end
+end
+
+return M
