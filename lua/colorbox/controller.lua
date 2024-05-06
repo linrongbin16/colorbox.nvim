5ef0657815971be4de372f2de4eb5eccdc4de27a
linrongbin16 <linrongbin16@outlook.com>
2024-05-06 10:01:40 +0800

    chore

--- lua/colorbox/controller.lua
+++ lua/colorbox/controller.lua
@@ -33,9 +33,6 @@ M._update = function()
       vim.inspect(packstart)
     )
   )
-  if uv.fs_stat(packstart) == nil then
-    vim.fn.mkdir(packstart, "p")
-  end
   vim.opt.packpath:append(home_dir)
 
   local HandleToColorSpecsMap = db.get_handle_to_color_specs_map()
